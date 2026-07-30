ed91b33ecd8dfa25927c010990f57779680d11eb
Lumin2002 <983603969@qq.com>
2026-07-30 22:07:07 +0800

    清理：移除临时辅助脚本（add_missing/check_new/fix_new_issues/quick_fix2/rm_inventory_log/verify_final）

--- add_missing.py
+++
@@ -1,7 +0,0 @@
-﻿lines = open("D:/PycharmProjects/cloth-system/order/views.py","r",encoding="utf-8").readlines()
-for i,l in enumerate(lines,1):
-    if "def inventory_import_start" in l:
-        lines.insert(i-1, "def inventory_import_page(request):\n    return render(request, \"order/inventory_import.html\")\n\n")
-        print(f"Added at L{i}")
-        break
-open("D:/PycharmProjects/cloth-system/order/views.py","w",encoding="utf-8").writelines(lines)
